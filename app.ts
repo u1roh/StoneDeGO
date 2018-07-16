4e6b3cd613d5e78ae0a3fea058af27a38dc69289
u1roh <terada.yuichiro+github@gmail.com>
2018-07-16 15:54:29 +0900

    vsComputer.html と vsPerson.html 等を追加

--- app.ts
+++ app.ts
@@ -35,26 +35,6 @@ for (let i = 0; i < stoneField.length; ++i) {
     }
 }
 
-const field = new Array<Array<number>>(ngrid);
-for (let i = 0; i < ngrid; ++i) {
-    field[i] = new Array<number>(ngrid);
-    for (let j = 0; j < ngrid; ++j) field[i][j] = 0;
-}
-
-let scoreRed = 0;
-let scoreBlue = 0;
-
-function updateScores() {
-    scoreRed = 0;
-    scoreBlue = 0;
-    for (let i = 0; i < ngrid; ++i) {
-        for (let j = 0; j < ngrid; ++j) {
-            if (field[i][j] > 0)++scoreRed;
-            if (field[i][j] < 0)++scoreBlue;
-        }
-    }
-}
-
 
 enum Stone {
     None,
@@ -62,67 +42,130 @@ enum Stone {
     Blue
 }
 
-const stones = new Array<Array<Stone>>(ngrid);
-for (let i = 0; i < ngrid; ++i) {
-    stones[i] = new Array<Stone>(ngrid);
-    for (let j = 0; j < ngrid; ++j) stones[i][j] = Stone.None;
-}
 
-function addStoneField(x: number, y: number, sgn: number) {
-    for (let i = 0; i < stoneField.length; ++i) {
-        for (let j = 0; j < stoneField.length; ++j) {
-            let ii = x + (i - breadth);
-            let jj = y + (j - breadth);
-            if (ii < 0) ii = -ii - 1;
-            if (jj < 0) jj = -jj - 1;
-            if (ii >= ngrid) ii = 2 * ngrid - ii - 1;
-            if (jj >= ngrid) jj = 2 * ngrid - jj - 1;
-            field[ii][jj] += sgn * stoneField[i][j];
-        }
-    }
-}
-
-function putStone(x: number, y: number, stone: Stone) : boolean {
-    const sgn = stone == Stone.Red ? +1 : stone == Stone.Blue ? -1 : 0;
-    if (stones[x][y] != Stone.None) return false;
-    if (sgn * field[x][y] <= -1) return false;
-    stones[x][y] = stone;
-    addStoneField(x, y, sgn);
-    let removal = true;
-    while (removal) {
-        removal = false;
+class GameBoard {
+    field: Array<Array<number>>;
+    stones: Array<Array<Stone>>;
+    scoreRed: number;
+    scoreBlue: number;
+    constructor() {
+        this.field = new Array<Array<number>>(ngrid);
+        this.stones = new Array<Array<Stone>>(ngrid);
         for (let i = 0; i < ngrid; ++i) {
+            this.field[i] = new Array<number>(ngrid);
+            this.stones[i] = new Array<Stone>(ngrid);
             for (let j = 0; j < ngrid; ++j) {
-                if (stones[i][j] == Stone.None || stones[i][j] == stone) continue;
-                if (-sgn * field[i][j] < 0) {
-                    stones[i][j] = Stone.None;
-                    addStoneField(i, j, sgn);
-                    removal = true;
-                }
+                this.field[i][j] = 0;
+                this.stones[i][j] = Stone.None;
             }
         }
     }
-    updateScores();
-    return true;
+    private updateScores() {
+        this.scoreRed = 0;
+        this.scoreBlue = 0;
+        for (let i = 0; i < ngrid; ++i) {
+            for (let j = 0; j < ngrid; ++j) {
+                if (this.field[i][j] > 0)++this.scoreRed;
+                if (this.field[i][j] < 0)++this.scoreBlue;
+            }
+        }
+    }
+    private addStoneField(x: number, y: number, sgn: number) {
+        for (let i = 0; i < stoneField.length; ++i) {
+            for (let j = 0; j < stoneField.length; ++j) {
+                let ii = x + (i - breadth);
+                let jj = y + (j - breadth);
+                if (ii < 0) ii = -ii - 1;
+                if (jj < 0) jj = -jj - 1;
+                if (ii >= ngrid) ii = 2 * ngrid - ii - 1;
+                if (jj >= ngrid) jj = 2 * ngrid - jj - 1;
+                this.field[ii][jj] += sgn * stoneField[i][j];
+            }
+        }
+    }
+    putStone(x: number, y: number, stone: Stone) : boolean {
+        const sgn = stone == Stone.Red ? +1 : stone == Stone.Blue ? -1 : 0;
+        if (this.stones[x][y] != Stone.None) return false;
+        if (sgn * this.field[x][y] <= -1) return false;
+        this.stones[x][y] = stone;
+        this.addStoneField(x, y, sgn);
+        let removal = true;
+        while (removal) {
+            removal = false;
+            for (let i = 0; i < ngrid; ++i) {
+                for (let j = 0; j < ngrid; ++j) {
+                    if (this.stones[i][j] == Stone.None || this.stones[i][j] == stone) continue;
+                    if (-sgn * this.field[i][j] < 0) {
+                        this.stones[i][j] = Stone.None;
+                        this.addStoneField(i, j, sgn);
+                        removal = true;
+                    }
+                }
+            }
+        }
+        this.updateScores();
+        return true;
+    }
+    score(stone: Stone): number {
+        return stone == Stone.Red ? this.scoreRed : this.scoreBlue;
+    }
+    countStones(stone: Stone): number {
+        let n = 0;
+        for (let i = 0; i < ngrid; ++i) {
+            for (let j = 0; j < ngrid; ++j) {
+                if (this.stones[i][j] == stone)++n;
+            }
+        }
+        return n;
+    }
+    clone(): GameBoard {
+        let dst = new GameBoard();
+        for (let i = 0; i < ngrid; ++i) {
+            for (let j = 0; j < ngrid; ++j) {
+                dst.field[i][j] = this.field[i][j];
+                dst.stones[i][j] = this.stones[i][j];
+            }
+        }
+        dst.scoreRed = this.scoreRed;
+        dst.scoreBlue = this.scoreBlue;
+        return dst;
+    }
 }
 
 enum Player { Person, Computer }
 let playerRed = Player.Person;
-//let playerBlue = Player.Computer;
-let playerBlue = Player.Person;
+let playerBlue = Player.Computer;
+//let playerBlue = Player.Person;
 
-function search(stone: Stone) : number[] {
+function search(game: GameBoard, stone: Stone) : number[] {
     function evaluate(i: number, j: number) : number {
-        return 0;
+        const copy = game.clone();
+        copy.putStone(i, j, stone);
+        const sgn = stone == Stone.Red ? -1 : +1;
+        const enemy = stone == Stone.Red ? Stone.Blue : Stone.Red;
+        let minScore = 1000;
+        for (let i = 0; i < ngrid; ++i) {
+            for (let j = 0; j < ngrid; ++j) {
+                if (copy.stones[i][j] != Stone.None) continue;
+                if (sgn * copy.field[i][j] < -1) continue;
+                const copy2 = copy.clone();
+                copy2.putStone(i, j, enemy);
+                const score1 = copy2.score(stone) - game.score(stone);
+                const score2 = copy2.countStones(stone) - game.countStones(stone) + game.countStones(enemy) - copy2.countStones(enemy);
+                const score = score1 + 40 * score2;
+                if (score < minScore) minScore = score;
+            }
+        }
+        return minScore;
     }
     const sgn = stone == Stone.Red ? +1 : -1;
     let x = 0;
     let y = 0;
-    let maxValue = 0;
+    let maxValue = -1000;
     for (let i = 0; i < ngrid; ++i) {
         for (let j = 0; j < ngrid; ++j) {
-            if (stones[i][j] != Stone.None) continue;
-            if (sgn * field[i][j] < -1) continue;
+            if (game.stones[i][j] != Stone.None) continue;
+            if (sgn * game.field[i][j] < -1) continue;
             const value = evaluate(i, j);
             if (value > maxValue) {
                 maxValue = value;
@@ -134,15 +177,15 @@ function search(stone: Stone) : number[] {
     return [x, y];
 }
 
-function drawTerritoryStatusBar(con: CanvasRenderingContext2D, y0: number, width: number, height: number) {
+function drawTerritoryStatusBar(con: CanvasRenderingContext2D, game: GameBoard, y0: number, width: number, height: number) {
     con.beginPath();
     con.rect(2, y0 + 2, width - 4, height - 4);
     con.fillStyle = "#000000";
     con.fill();
     con.closePath();
 
-    let widthRed  = (width - 4) * scoreRed  / (ngrid * ngrid);
-    let widthBlue = (width - 4) * scoreBlue / (ngrid * ngrid);
+    let widthRed  = (width - 4) * game.scoreRed  / (ngrid * ngrid);
+    let widthBlue = (width - 4) * game.scoreBlue / (ngrid * ngrid);
     con.beginPath();
     con.rect(2, y0 + 2, widthRed, height - 4);
     con.fillStyle = "#ff0000";
@@ -157,6 +200,8 @@ function drawTerritoryStatusBar(con: CanvasRenderingContext2D, y0: number, width
 
 
 window.onload = () => {
+    const game = new GameBoard();
+
     const canvas = <HTMLCanvasElement>document.getElementById("gameCanvas");
     const con = canvas.getContext("2d");
 
@@ -169,14 +214,14 @@ window.onload = () => {
     canvas.height = size + statusBarHeight + 10;
 
     let turn = 0;
-    let cursorX = 0;
-    let cursorY = 0;
+    let cursorX = -1;
+    let cursorY = -1;
 
     function onPutStone() {
         ++turn;
         draw();
         if (turn == totalTurn) {
-            const winner = scoreRed > scoreBlue ? "RED" : "BLUE";
+            const winner = game.scoreRed > game.scoreBlue ? "RED" : "BLUE";
             alert(winner + " WIN!!!");
             document.location.reload();
         }
@@ -194,14 +239,14 @@ window.onload = () => {
     canvas.onclick = e => {
         const x = Math.floor((e.x - canvas.offsetLeft) * ngrid / size);
         const y = Math.floor((e.y - canvas.offsetTop)  * ngrid / size);
-        if (putStone(x, y, turn % 2 == 0 ? Stone.Red : Stone.Blue)) {
+        if (game.putStone(x, y, turn % 2 == 0 ? Stone.Red : Stone.Blue)) {
             onPutStone();
 
             const nextPlayer = turn % 2 == 0 ? playerRed : playerBlue;
             if (nextPlayer == Player.Computer) {
                 const stone = turn % 2 == 0 ? Stone.Red : Stone.Blue;
-                const pos = search(stone);
-                if (putStone(pos[0], pos[1], stone)) {
+                const pos = search(game, stone);
+                if (game.putStone(pos[0], pos[1], stone)) {
                     onPutStone();
                 }
             }
@@ -215,7 +260,7 @@ window.onload = () => {
             for (let j = 0; j < ngrid; ++j) {
                 con.beginPath();
                 con.rect(size * i / ngrid, size * j / ngrid, size / ngrid, size / ngrid);
-                con.fillStyle = makeFieldColor(field[i][j]);
+                con.fillStyle = makeFieldColor(game.field[i][j]);
                 con.fill();
                 con.closePath();
             }
@@ -224,17 +269,20 @@ window.onload = () => {
         const radius = 0.3 * canvas.width / ngrid;
         for (let i = 0; i < ngrid; ++i) {
             for (let j = 0; j < ngrid; ++j) {
-                if (stones[i][j] == Stone.None) continue;
+                if (game.stones[i][j] == Stone.None) continue;
                 con.beginPath();
                 con.arc(size * (i + 0.5) / ngrid, size * (j + 0.5) / ngrid, radius, 0, 2 * Math.PI);
-                con.fillStyle = stones[i][j] == Stone.Red ? "#ff4040" : "#4040ff";
+                con.fillStyle = game.stones[i][j] == Stone.Red ? "#ff4040" : "#4040ff";
                 con.strokeStyle = "#000000";
                 con.fill();
                 con.stroke();
                 con.closePath();
             }
         }
-        if (stones[cursorX][cursorY] == Stone.None && (turn % 2 == 0 ? +1 : -1) * field[cursorX][cursorY] > -1) {
+        if (0 <= cursorX && cursorX < ngrid &&
+            0 <= cursorY && cursorY < ngrid &&
+            game.stones[cursorX][cursorY] == Stone.None &&
+            (turn % 2 == 0 ? +1 : -1) * game.field[cursorX][cursorY] > -1) {
             con.beginPath();
             con.arc(size * (cursorX + 0.5) / ngrid, size * (cursorY + 0.5) / ngrid, radius, 0, 2 * Math.PI);
             con.strokeStyle = turn % 2 == 0 ? "#ff4040" : "#4040ff";
@@ -259,7 +307,7 @@ window.onload = () => {
         con.closePath();
 
         // status bar
-        drawTerritoryStatusBar(con, size + 10, size, statusBarHeight);
+        drawTerritoryStatusBar(con, game, size + 10, size, statusBarHeight);
     }
 
     draw();
